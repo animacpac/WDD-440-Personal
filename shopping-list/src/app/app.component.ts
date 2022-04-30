977946a2e7ce22636198cd63c00bc02a97ea706e
animacpac <56101332+animacpac@users.noreply.github.com>
2022-04-29 22:34:26 -0600

    Shopping Project

--- shopping-list/src/app/app.component.ts
+++ shopping-list/src/app/app.component.ts
@@ -4,15 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
- 
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-
-  onToggleDetails(){
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date());
-  }
+  title = 'shopping-list';
 }
