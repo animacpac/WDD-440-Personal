2db5dc3b6c765b82a4d35d1bc2620b6b1d05356f
animacpac <56101332+animacpac@users.noreply.github.com>
2022-05-07 18:28:18 -0600

    Fixed

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contact/contact-item/contact-item.component';
+import { ContactEditComponent } from './contact/contact-edit/contact-edit.component';
 
 
 @NgModule({
@@ -15,6 +17,8 @@ import { ContactDetailComponent } from './contact/contact-detail/contact-detail.
     ContactComponent,
     ContactListComponent,
     ContactDetailComponent,
+    ContactItemComponent,
+    ContactEditComponent,
 
   ],
   imports: [
