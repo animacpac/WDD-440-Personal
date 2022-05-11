454f11275309aae60ed2a2b02f9d40f7a0c5cd22
animacpac <56101332+animacpac@users.noreply.github.com>
2022-05-10 18:34:28 -0600

    Document

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -8,6 +8,15 @@ import { ContactListComponent } from './contact/contact-list/contact-list.compon
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import { ContactItemComponent } from './contact/contact-item/contact-item.component';
 import { ContactEditComponent } from './contact/contact-edit/contact-edit.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
 
 @NgModule({
@@ -19,6 +28,15 @@ import { ContactEditComponent } from './contact/contact-edit/contact-edit.compon
     ContactDetailComponent,
     ContactItemComponent,
     ContactEditComponent,
+    DocumentsComponent,
+    MessagesComponent,
+    DocumentDetailComponent,
+    DocumentEditComponent,
+    DocumentItemComponent,
+    DocumentListComponent,
+    MessageEditComponent,
+    MessageItemComponent,
+    MessageListComponent,
 
   ],
   imports: [
