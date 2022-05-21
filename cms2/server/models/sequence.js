a9fc87f86499be5aff4d4b1517fc497486a165e9
animacpac <56101332+animacpac@users.noreply.github.com>
2022-05-20 22:12:11 -0600

    Cleanup

--- cms2/server/models/sequence.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const sequenceSchema = mongoose.Schema({
-    maxDocumentId: { type: String },
-    maxMessageId: { type: String },
-    maxContactId: { type: String }
- });
- 
- module.exports = mongoose.model('Sequence', sequenceSchema);
