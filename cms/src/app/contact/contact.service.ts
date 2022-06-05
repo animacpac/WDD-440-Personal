6b62ac1a0a9a02fdf0926159c871f0f9c1bd13e2
animacpac <56101332+animacpac@users.noreply.github.com>
2022-06-04 20:15:37 -0600

    Finished Contact

--- cms/src/app/contact/contact.service.ts
+++ cms/src/app/contact/contact.service.ts
@@ -11,26 +11,88 @@ export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact[]>();
   contactChangedEvent = new EventEmitter<Contact[]>();
   contactListChangedEvent = new Subject<Contact[]>();
+  Subject: Contact[] = [];
+
+  maxContactId: number;
+
 
   constructor() { 
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
 
   getContacts(): Contact[] {
     return this.contacts.slice();
   }
+  
+
 
   getContact(id: string): Contact | null{
-    return this.contacts.find((contact) => contact.id === id);
-    // for (let i = 0; i < this.contacts.length; i++) {
-    //   if(this.contacts[i].id == id){
-    //     return this.contacts[i];
-    //   }
+    
+    for (let i = 0; i < this.contacts.length; i++) {
+      if(this.contacts[i].id == id){
+        return this.contacts[i];
+      }
       
-    // }
-    // return null;
+    }
+    return null;
        
    }
+   deleteDocument(contact: Contact) {
+    if (!contact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(contact);
+    if (pos < 0) {
+      return;
+    };
+    this.contacts.splice(pos, 1);
+    this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+   getMaxId() {
+
+    let maxId = 0;
+
+    for (const contact of this.contacts) {
+      const currentId = +contact.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+
+    return maxId;
+  }
+
+
+   addContact(newContact: Contact){
+
+    if (!newContact) {
+      return;
+    }
+     
+    this.maxContactId++
+    newContact.id = this.maxContactId.toString();
+    this.contacts.push(newContact)
+    const contactsListClone = this.contacts.slice()
+    this.contactListChangedEvent.next(contactsListClone)
+   }
+
+   updateDocument(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact)
+        return
+  
+    let pos = this.contacts.indexOf(originalContact)
+    if(pos < 0){
+      return
+    }
+  
+    newContact.id = originalContact.id
+    this.contacts[pos] = newContact
+    const documentsListClone = this.contacts.slice()
+    this.contactListChangedEvent.next(documentsListClone)
+  }
+
    deleteContact(contact: Contact) {
     if (!contact) {
        return;
